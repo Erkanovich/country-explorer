3520fd6aefb4584e14de5b3efc61c6d008ceb0f7
Erik Niklasson <erik.niklasson@wow-group.com>
2020-08-26 18:02:35 +0200

    added more information to the country cards

--- src/country-explorer/country-card/country-card.js
+++ src/country-explorer/country-card/country-card.js
@@ -5,4 +5,7 @@ export class CountryCard {
     @bindable imageUrl;
     @bindable name = 'Country name not available';
     @bindable nativeName = 'Native name not available';
+    @bindable region = 'Region not available';
+    @bindable capital = 'Capital not available';
+    @bindable population = 'Population not available';
 }
